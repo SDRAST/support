220c1d1b724f95af9e92608979157475d39e0617
Tom Kuiper <kuiper@jpl.nasa.gov>
2017-09-07 09:59:04 -0700

    Added to local_dirs

--- local_dirs.py
+++ local_dirs.py
@@ -3,11 +3,16 @@ paths to local packages and data
 """
 local_packages = "/usr/local/lib/python2.7/DSN-Sci-packages/"
 
+auto_pkg_dir = local_packages+"Automation/"
+auto_apps_path = auto_pkg_dir + "apps/"
+
 data_dir = "/usr/local/RA_data/"
 fits_dir = data_dir+"FITS/"
 hdf5_dir = data_dir+"HDF5/"
 
 projects_dir = "/usr/local/projects/"
-auto_dir     = projects_dir+"DSAO"
+auto_dir     = projects_dir+"DSAO/"
+sci_proj_path = auto_dir+"Science/"
+act_proj_path = auto_dir+"Activities/"
 
 project_data_dir = "/usr/local/project_data/"
