33b3cd872fb49f5fba8e8fb64c729bcce53311f9
Dean C Shaff <dean.c.shaff@jpl.nasa.gov>
2017-10-23 12:42:42 +0400

    Restructured Ephem.py into module

---
+++ Ephem/__init__.py
@@ -0,0 +1,122 @@
+# -*- coding: utf-8 -*-
+"""
+Module Ephem - extends module pyephem for radio astronomy
+
+Module Ephem extends module ephem (package pyephem).  Everything from
+module 'ephem' is inherited.  In addition, it provides the classes
+Quasar(), Pulsar() and DSS().  It also provides pulsar physical
+data and quasar fluxes.  Use like this:
+>>> from Astronomy import Ephem
+>>> x = Ephem.Pulsar('J1809-0743')
+
+Notes
+=====
+module ephem
+------------
+http://rhodesmill.org/brandon/projects/pyephem-manual.html
+
+About Celestial Coordinates
+---------------------------
+a_ra, a_dec — Astrometric geocentric position for epoch (e.g. J2000)
+g_ra, g_dec — Apparent geocentric position for date specified in
+              the compute(), after correcting for precession, relativistic
+              deflection, nutation and aberration
+ra, dec     - apparent topocentric position, after correction for parallax
+              and refraction. Set the Observer() attribute pressure to zero if
+              you want PyEphem to ignore the effects of atmospheric refraction
+
+Diagnostics
+-----------
+To see what this module does internally:
+>>> from Astronomy import Ephem
+>>> Ephem.diag = True
+>>> x = Ephem.Pulsar('J1809-0743')
+"""
+import logging
+import datetime
+from math import pi
+
+from ephem import Date, FixedBody, Observer
+from matplotlib.dates import date2num
+
+from Astrophysics.Pulsars import pulsar_data as PD
+from Radio_Astronomy import michigan, vla_cal
+
+from .pulsar import Pulsar
+from .quasar import Quasar
+from .dss import DSS
+
+module_logger = logging.getLogger(__name__)
+
+J2000 = Date("2000/1/1 00:00:00")
+
+diag = False
+
+# planets recognized by module ephem
+Planets = ['Jupiter', 'Mars', 'Mercury', 'Moon', 'Neptune', 'Pluto',
+           'Saturn', 'Sun', 'Uranus', 'Venus']
+
+Jnames = PD.data.keys() # pulsar Julian epoch names
+Jnames.sort()
+
+cal_dict = vla_cal.get_cal_dict() # VLA calibrators
+Bname_dict, cat_3C_dict = vla_cal.VLA_name_xref(cal_dict) # name dictionaries
+Bnames = vla_cal.Jnames_to_B(Bname_dict) # B names keyed on J names
+
+def remove_item(thislist, item):
+  """
+  Remove item from list without complaining if absent
+
+  @param thislist : list of items
+  @param item : item which may or may not be in the list
+
+  @return: reduced list
+  """
+  try:
+    thislist.remove(item)
+  except ValueError:
+    pass
+  return thislist
+
+class EphemException(Exception):
+  """
+  Exception class for Ephem module
+  """
+  def __init__(self, value=None, details=None):
+    """
+    Creates an instance of EphemException()
+    """
+    if value:
+      self.value = value
+    if details:
+      self.details = details
+  def __str__(self):
+    """
+    EphemException() instance message
+    """
+    msg = "error"
+    if self.value:
+      msg += ": " + repr(self.value)
+    if self.details:
+      msg += ", " + self.details
+    return repr(msg)
+
+def calibrator(name):
+  """
+  Creates an Ephem (pyephem) Body() instance for a flux calibrator
+  """
+  try:
+    # planet?
+    Planets.index(name)
+    exec('calibrator = '+name+'()')
+    return calibrator
+  except Exception, details:
+    if diag:
+      print "Not a planet:",details
+    # quasar?
+    try:
+      calibrator = Quasar(name)
+      return calibrator
+    except Exception, details:
+      print "Not a quasar:",details
+      return None
---
+++ Ephem/dss.py
@@ -0,0 +1,39 @@
+import logging
+
+from ephem import Observer
+
+import Astronomy
+
+dsn = Astronomy.get_geodetic_coords()
+xyz = Astronomy.get_cartesian_coordinates()
+
+module_logger = logging.getLogger(__name__)
+
+class DSS(Observer):
+  """
+  Observer class for DSN stations
+  """
+  def __init__(self, number):
+    """
+    Creates an instance of a DSN station Observer()
+
+    Attributes
+    ==========
+    lon       - east longitude (degrees)
+    lat       - north latitude (degrees)
+    elevation - altitude (meters)
+    timezone  - difference (hours) from UTC
+    name      -
+    diam      - diameter (m)
+
+    @param number : station number
+    """
+    super(DSS,self).__init__()
+    module_logger.debug("Initiating DSS(%d)", number)
+    self.lon = -dsn[number][0]*pi/180.
+    self.lat = dsn[number][1]*pi/180.
+    self.elevation = dsn[number][2]
+    self.timezone = dsn[number][3]
+    self.name = dsn[number][4]
+    self.diam = dsn[number][5]
+    self.xyz = xyz["DSS %2d" % number]
