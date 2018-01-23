11c3152ba13b7936756b5d66a55aea003bca7f83
Dean C Shaff <dean.c.shaff@jpl.nasa.gov>
2018-01-23 13:27:50 +0400

    Changed Logs to logs in local_dirs.py

--- local_dirs.py
+++ local_dirs.py
@@ -19,7 +19,6 @@ act_proj_path = auto_dir+"Activities/"
 
 project_data_dir = "/usr/local/project_data/"
 
-log_dir = "/usr/local/Logs/"+gethostname()+"/"
+log_dir = "/usr/local/logs/"+gethostname()+"/"
 wvsr_dir = "/data/"   # was data2 on crab14
 wvsr_fft_dir = wvsr_dir+"post_processing/auto/" # was cjnaudet/auto on crab14
-
