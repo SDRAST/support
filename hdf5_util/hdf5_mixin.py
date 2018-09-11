d7cea156a9d22600427da231037dd60846299aba
Dean Shaff <dean.shaff@gmail.com>
2018-09-11 07:15:42 -0500

    changed naming convention

--- hdf5_util/hdf5_mixin.py
+++ hdf5_util/hdf5_mixin.py
@@ -59,7 +59,9 @@ class HDF5Mixin(object):
             self.data_file_obj.close()
         # use current date in file name and path
         now = datetime.datetime.utcnow()
-        self.data_file_name = self.name + now.strftime("-%Y-%j-%H%M%S.hdf5")
+        self.data_file_name = "{}.{}.hdf5".format(
+            self.name, now.strftime("%Y-%j-%H%M%S")
+        )
         module_logger.debug("open_data_file {}".format(
             self.data_file_name
         ))
